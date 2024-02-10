e145897fdf3d3225b1588bad1212fcae3d8f1d4a
Jagannath8 <jagannath9611.com>
2024-02-10 20:08:10 +0530

    Day 10

---
+++ Day10/day10.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+function staticFileServer(req, res) {
+    // Your implementation here
+    const publicPath = path.join(__dirname, "public");
+    express.static(publicPath)(req, res, () => {
+        res.status(404).send("Not Found");
+    });
+
+    if(req.url === "/"){
+    }
+}
+
+app.use(staticFileServer);
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
